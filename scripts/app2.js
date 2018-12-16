02f455ea1d4383a47bc650002048b70b02bec67f
Aquib Ansari <aquibxv@gmail.com>
2018-12-16 13:29:36 +0530

    Add files via upload

---
+++ scripts/app2.js
@@ -0,0 +1,346 @@
+
+(function() {
+  'use strict';
+
+  var injectedForecast = {
+    key: 'newyork',
+    label: 'New York, NY',
+    currently: {
+      time: 1453489481,
+      summary: 'Clear',
+      icon: 'partly-cloudy-day',
+      temperature: 52.74,
+      apparentTemperature: 74.34,
+      precipProbability: 0.20,
+      humidity: 0.77,
+      windBearing: 125,
+      windSpeed: 1.52
+    },
+    daily: {
+      data: [
+        {icon: 'clear-day', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'rain', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'snow', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'sleet', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'fog', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'wind', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'partly-cloudy-day', temperatureMax: 55, temperatureMin: 34}
+      ]
+    }
+  };
+
+  var weatherAPIUrlBase = 'https://publicdata-weather.firebaseio.com/';
+
+  var app = {
+    isLoading: true,
+    visibleCards: {},
+    selectedCities: [],
+    spinner: document.querySelector('.loader'),
+    cardTemplate: document.querySelector('.cardTemplate'),
+    container: document.querySelector('.main'),
+    addDialog: document.querySelector('.dialog-container'),
+    daysOfWeek: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+  };
+
+  /*****************************************************************************
+   *
+   * Event listeners for UI elements
+   *
+   ****************************************************************************/
+
+  /* Event listener for refresh button */
+  document.getElementById('butRefresh').addEventListener('click', function() {
+    app.updateForecasts();
+  });
+
+  /* Event listener for add new city button */
+  document.getElementById('butAdd').addEventListener('click', function() {
+    // Open/show the add new city dialog
+    app.toggleAddDialog(true);
+  });
+
+  /* Event listener for add city button in add city dialog */
+  document.getElementById('butAddCity').addEventListener('click', function() {
+    var select = document.getElementById('selectCityToAdd');
+    var selected = select.options[select.selectedIndex];
+    var key = selected.value;
+    var label = selected.textContent;
+    if (!app.selectedCities) {
+      app.selectedCities = [];
+    }
+    app.getForecast(key, label);
+    app.selectedCities.push({key: key, label: label});
+    app.saveSelectedCities();
+    app.toggleAddDialog(false);
+  });
+
+  /* Event listener for cancel button in add city dialog */
+  document.getElementById('butAddCancel').addEventListener('click', function() {
+    app.toggleAddDialog(false);
+  });
+
+
+  /*****************************************************************************
+   *
+   * Methods to update/refresh the UI
+   *
+   ****************************************************************************/
+
+  // Toggles the visibility of the add new city dialog.
+  app.toggleAddDialog = function(visible) {
+    if (visible) {
+      app.addDialog.classList.add('dialog-container--visible');
+    } else {
+      app.addDialog.classList.remove('dialog-container--visible');
+    }
+  };
+
+  // Updates a weather card with the latest weather forecast. If the card
+  // doesn't already exist, it's cloned from the template.
+  app.updateForecastCard = function(data) {
+    var card = app.visibleCards[data.key];
+    if (!card) {
+      card = app.cardTemplate.cloneNode(true);
+      card.classList.remove('cardTemplate');
+      card.querySelector('.location').textContent = data.label;
+      card.removeAttribute('hidden');
+      app.container.appendChild(card);
+      app.visibleCards[data.key] = card;
+    }
+    card.querySelector('.description').textContent = data.currently.summary;
+    card.querySelector('.date').textContent =
+      new Date(data.currently.time * 1000);
+    card.querySelector('.current .icon').classList.add(data.currently.icon);
+    card.querySelector('.current .temperature .value').textContent =
+      Math.round(data.currently.temperature);
+    card.querySelector('.current .feels-like .value').textContent =
+      Math.round(data.currently.apparentTemperature);
+    card.querySelector('.current .precip').textContent =
+      Math.round(data.currently.precipProbability * 100) + '%';
+    card.querySelector('.current .humidity').textContent =
+      Math.round(data.currently.humidity * 100) + '%';
+    card.querySelector('.current .wind .value').textContent =
+      Math.round(data.currently.windSpeed);
+    card.querySelector('.current .wind .direction').textContent =
+      data.currently.windBearing;
+    var nextDays = card.querySelectorAll('.future .oneday');
+    var today = new Date();
+    today = today.getDay();
+    for (var i = 0; i < 7; i++) {
+      var nextDay = nextDays[i];
+      var daily = data.daily.data[i];
+      if (daily && nextDay) {
+        nextDay.querySelector('.date').textContent =
+          app.daysOfWeek[(i + today) % 7];
+        nextDay.querySelector('.icon').classList.add(daily.icon);
+        nextDay.querySelector('.temp-high .value').textContent =
+          Math.round(daily.temperatureMax);
+        nextDay.querySelector('.temp-low .value').textContent =
+          Math.round(daily.temperatureMin);
+      }
+    }
+    if (app.isLoading) {
+      app.spinner.setAttribute('hidden', true);
+      app.container.removeAttribute('hidden');
+      app.isLoading = false;
+    }
+  };
+
+
+  /*****************************************************************************
+   *
+   * Methods for dealing with the model
+   *
+   ****************************************************************************/
+
+  // Gets a forecast for a specific city and update the card with the data
+  app.getForecast = function(key, label) {
+    var statement = 'select * from weather.forecast where woeid=' + key;
+    var url = weatherAPIUrlBase + key + '.json';
+
+    // TODO add cache logic here
+    if ('caches' in window) {
+      /*
+       * Check if the service worker has already cached this city's weather
+       * data. If the service worker has the data, then display the cached
+       * data while the app fetches the latest data.
+       */
+      caches.match(url).then(function(response) {
+        if (response) {
+          response.json().then(function updateFromCache(json) {
+            var results = json.query.results;
+            results.key = key;
+            results.label = label;
+            results.created = json.query.created;
+            app.updateForecastCard(results);
+          });
+        }
+      });
+    }
+
+    // Fetch the latest
+    // Make the XHR to get the data, then update the card
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+          var response = JSON.parse(request.response);
+          response.key = key;
+          response.label = label;
+          app.updateForecastCard(response);
+        }
+      }
+    };
+    request.open('GET', url);
+    request.send();
+  };
+
+  // Iterate all of the cards and attempt to get the latest forecast data
+  app.updateForecasts = function() {
+    var keys = Object.keys(app.visibleCards);
+    keys.forEach(function(key) {
+      app.getForecast(key);
+    });
+  };
+
+  
+  // TODA add SavedSelectedCitites function here
+  // saving the list of cities into local storage
+  app.saveSelectedCities = function() {
+    var selectedCities = JSON.stringify(app.selectedCities);
+    localStorage.selectedCities = selectedCities;
+  };
+
+  app.getIconClass = function(weatherCode) {
+    // Weather codes: https://developer.yahoo.com/weather/documentation.html#codes
+    weatherCode = parseInt(weatherCode);
+    switch (weatherCode) {
+      case 25: // cold
+      case 32: // sunny
+      case 33: // fair (night)
+      case 34: // fair (day)
+      case 36: // hot
+      case 3200: // not available
+        return 'clear-day';
+      case 0: // tornado
+      case 1: // tropical storm
+      case 2: // hurricane
+      case 6: // mixed rain and sleet
+      case 8: // freezing drizzle
+      case 9: // drizzle
+      case 10: // freezing rain
+      case 11: // showers
+      case 12: // showers
+      case 17: // hail
+      case 35: // mixed rain and hail
+      case 40: // scattered showers
+        return 'rain';
+      case 3: // severe thunderstorms
+      case 4: // thunderstorms
+      case 37: // isolated thunderstorms
+      case 38: // scattered thunderstorms
+      case 39: // scattered thunderstorms (not a typo)
+      case 45: // thundershowers
+      case 47: // isolated thundershowers
+        return 'thunderstorms';
+      case 5: // mixed rain and snow
+      case 7: // mixed snow and sleet
+      case 13: // snow flurries
+      case 14: // light snow showers
+      case 16: // snow
+      case 18: // sleet
+      case 41: // heavy snow
+      case 42: // scattered snow showers
+      case 43: // heavy snow
+      case 46: // snow showers
+        return 'snow';
+      case 15: // blowing snow
+      case 19: // dust
+      case 20: // foggy
+      case 21: // haze
+      case 22: // smoky
+        return 'fog';
+      case 24: // windy
+      case 23: // blustery
+        return 'windy';
+      case 26: // cloudy
+      case 27: // mostly cloudy (night)
+      case 28: // mostly cloudy (day)
+      case 31: // clear (night)
+        return 'cloudy';
+      case 29: // partly cloudy (night)
+      case 30: // partly cloudy (day)
+      case 44: // partly cloudy
+        return 'partly-cloudy-day';
+    }
+  };
+
+  /*
+   * Fake weather data that is presented when the user first uses the app,
+   * or when the user has not saved any cities. See startup code for more
+   * discussion.
+   */
+  var initialWeatherForecast = {
+    key: '2459115',
+    label: 'New York, NY',
+    created: '2016-07-22T01:00:00Z',
+    channel: {
+      astronomy: {
+        sunrise: "5:43 am",
+        sunset: "8:21 pm"
+      },
+      item: {
+        condition: {
+          text: "Windy",
+          date: "Thu, 21 Jul 2016 09:00 PM EDT",
+          temp: 56,
+          code: 24
+        },
+        forecast: [
+          {code: 44, high: 86, low: 70},
+          {code: 44, high: 94, low: 73},
+          {code: 4, high: 95, low: 78},
+          {code: 24, high: 75, low: 89},
+          {code: 24, high: 89, low: 77},
+          {code: 44, high: 92, low: 79},
+          {code: 44, high: 89, low: 77}
+        ]
+      },
+      atmosphere: {
+        humidity: 56
+      },
+      wind: {
+        speed: 25,
+        direction: 195
+      }
+    }
+  };
+  app.updateForecastCard(injectedForecast);
+
+  // TODO add startup code here
+  app.selectedCities = localStorage.selectedCities;
+  if (app.selectedCities) {
+    app.selectedCities = JSON.parse(app.selectedCities);
+    app.selectedCities.forEach(function(city) {
+      app.getForecast(city.key, city.label);
+    });
+  } else {
+    /* The user is using the app for the first time, or the user has not
+     * saved any cities, so show the user some fake data. A real app in this
+     * scenario could guess the user's location via IP lookup and then inject
+     * that data into the page.
+     */
+    app.updateForecastCard(initialWeatherForecast);
+    app.selectedCities = [
+      {key: initialWeatherForecast.key, label: initialWeatherForecast.label}
+    ];
+    app.saveSelectedCities();
+  }
+
+  // TODO add service worker code here
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('../service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
+})();
